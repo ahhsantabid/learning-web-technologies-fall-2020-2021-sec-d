d4f7c41050e06be8277eb6d9f14b425f7fbf0159
unknown <you@example.com>
2020-11-04 22:51:03 +0600

    gender

---
+++ LAB_TASK_3.2[HTMI FORM]_php/gender_test.php
@@ -0,0 +1,18 @@
+<?php
+if(!isset($_REQUEST['gender'])){
+		header('location:gender?msg=errorinput');
+	}
+
+	if($_REQUEST['gender']=="Male"){
+		echo $_REQUEST['gender'];
+	}
+
+	if($_REQUEST['gender']=="Female"){
+		echo $_REQUEST['gender'];
+	}
+
+	if($_REQUEST['gender']=="Other"){
+		echo $_REQUEST['gender'];
+	}
+
+?>
