715dd3b7dd2a35b8ff7508d22e2add9d5a7883ba
unknown <you@example.com>
2020-11-08 19:41:52 +0600

    modified first commit

---
+++ LAB_TASk_4.1_SESSION/index.php
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+        <title>Home Page</title>
+
+
+
+</head>
+<body>
+    <style> 
+        table,tr,th {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+<form action="" method="POST">
+
+
+    <table border="1" width="600" align="center" cellpadding="15">
+    <tr>
+        <td align="left" colspan="3" style="border-right:0 ; padding: 5px 15px;">
+            <img src="home_logo.png" alt="home_logo" width="150" height="50">
+
+        </td>
+        <td align="right" style="border-left:0 ;" >
+                  <a href="index.php">Home</a>|
+                  <a href="login.php">Login</a>|
+                  <a href="registration.php">Registration</a>
+        </td>
+
+    </tr >
+    <tr >
+                <td  colspan="2" align="center"  style="border-right: 0;" ><h2>Welcome to XCompany</h2></td>
+    </tr>
+
+
+<tr >
+    <td align="center" colspan="2" width="30px" style=" padding: 8px 15px;border-right: 0;">Copyright &copy; 2017</td>
+</tr>
+
+
+    </table>
+    </form>
+</body>
+</html>
